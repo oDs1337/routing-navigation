3733aff0cde61539d76833fae5aa243b61f63858
oDs <odstobiaszcod@gmail.com>
2022-04-26 12:34:14 +0200

    Added title and input which fetch name from user and emit it

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  @Output() nameChanged = new EventEmitter<string>()
+
+  title: string = "Howdy, enter your name here:"
+  name: string = "";
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeName(value: string){
+    this.nameChanged.emit(value);
+    this.name = value;
   }
 
 }
